bb3ae8a040eca572b9dccc273b37733fa212a5e5
rcuevass <rogercuevas.ca@gmail.com>
2020-07-21 20:00:34 -0400

    refactoring and updating cml.yaml

--- src/get_data.py
+++ src/get_data.py
@@ -13,7 +13,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
 # Save it
 if not os.path.isdir("data"):
     os.mkdir("data")
-np.savetxt("data/train_features.csv", X_train)
-np.savetxt("data/test_features.csv", X_test)
-np.savetxt("data/train_labels.csv", y_train)
-np.savetxt("data/test_labels.csv", y_test)
+np.savetxt("../data/train_features.csv", X_train)
+np.savetxt("../data/test_features.csv", X_test)
+np.savetxt("../data/train_labels.csv", y_train)
+np.savetxt("../data/test_labels.csv", y_test)
--- src/train.py
+++ src/train.py
@@ -6,10 +6,10 @@ import os
 import numpy as np
 
 # Read in data
-X_train = np.genfromtxt("data/train_features.csv")
-y_train = np.genfromtxt("data/train_labels.csv")
-X_test = np.genfromtxt("data/test_features.csv")
-y_test = np.genfromtxt("data/test_labels.csv")
+X_train = np.genfromtxt("../data/train_features.csv")
+y_train = np.genfromtxt("../data/train_labels.csv")
+X_test = np.genfromtxt("../data/test_features.csv")
+y_test = np.genfromtxt("../data/test_labels.csv")
 
 # Fit a model
 depth = 10
@@ -21,11 +21,11 @@ acc = clf.score(X_test, y_test)
 print('Accuracy ', acc)
 print('Depth ', depth)
 print('Number of estimators ', num_estimators)
-with open("metrics.txt", 'w') as outfile:
+with open("../output_metrics/metrics.txt", 'w') as outfile:
     outfile.writelines(["Accuracy: " + str(acc) + "\n",
                         "Depth: " + str(depth) + "\n",
                         "Number estimators: " + str(num_estimators) + "\n"])
 
 # Plot it
 disp = plot_confusion_matrix(clf, X_test, y_test, normalize='true', cmap=plt.cm.Blues)
-plt.savefig('confusion_matrix.png')
+plt.savefig('../output_metrics/confusion_matrix.png')
