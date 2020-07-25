32d2387548f330e4e893a6edb4909f69f14d5dbb
rcuevass <rogercuevas.ca@gmail.com>
2020-07-24 20:47:42 -0400

    adding line to cml.yaml; creting .txt with entities saved to it

--- src/ner_simple.py
+++ src/ner_simple.py
@@ -17,9 +17,15 @@ log.info('small English NER spacy model has been loaded...')
 text = "When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously."
 doc = nlp(text)
 
+
+with open("..\\output_ner\\raw_entities_identified.txt", 'w') as outfile:
+    for ent in doc.ents:
+        ent_whole = str(ent.text) + ' ' + str(ent.label_) + ' ' + str(ent.start_char) + ' ' + str(ent.end_char) + "\n"
+        outfile.writelines(ent_whole)
+
 html_ner = spacy.displacy.render(doc, style="ent", jupyter=False)
 log.info('Generating NER image in HTML format...')
-output_path = Path(os.path.join("spacy_images/", "ner_sentence.html"))
+output_path = Path(os.path.join("..\\spacy_images\\", "ner_sentence.html"))
 log.info('Saving HTML NER image to file...')
 output_path.open('w', encoding="utf-8").write(html_ner)
 log.info('HTML NER image has been saved to file...')
